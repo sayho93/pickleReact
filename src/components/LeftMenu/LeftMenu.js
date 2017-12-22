48b37fd6cbbad5eebae5cb21c7819c35efe5800b
sayhoChun <fishcreek@naver.com>
2017-12-23 02:06:26 +0900

    hotfix) left menu collapse added

--- src/components/LeftMenu/LeftMenu.js
+++ src/components/LeftMenu/LeftMenu.js
@@ -1,5 +1,10 @@
 import React from "react";
-import {Nav, NavItem, NavLink} from "reactstrap";
+import {
+    Nav,
+    NavItem,
+    NavLink,
+    Collapse
+} from "reactstrap";
 import "./LeftMenu.css";
 import IUser from "react-icons/lib/ti/user";
 import INotice from "react-icons/lib/md/event-note";
@@ -7,14 +12,50 @@ import IStat from "react-icons/lib/ti/chart-bar";
 import ISetting from "react-icons/lib/fa/cog";
 
 class LeftMenu extends React.Component{
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            notice: false,
+            statistics: false,
+        };
+    }
+
+    // toggle() {
+    //     this.setState({ collapse: !this.state.collapse });
+    // };
+
+    toggle = (type) => {
+        // this.setState({{type}: !this.state.type});
+        let typeString = type;
+        switch(typeString){
+            case "notice":
+                this.setState({notice: !this.state.notice});
+                break;
+            case "statistics":
+                this.setState({statistics: !this.state.statistics});
+        }
+    };
+
     render(){
         return (
             <div className="navArea">
-                <Nav vertical>
-                    <NavItem className="navItem"><IUser/>User Manage</NavItem>
-                    <NavItem className="navItem"><INotice/>Notice Manage</NavItem>
-                    <NavItem className="navItem"><IStat/>통계</NavItem>
-                    <NavItem className="navItem"><ISetting/>관리자계정</NavItem>
+                <Nav vertical  className="flex-column">
+                    <NavItem className="navItem"><IUser/> User Manage</NavItem>
+                    <NavItem className="navItem" onClick={() => this.toggle("notice")}><INotice/> Notice Manage</NavItem>
+                    <Collapse isOpen={this.state.notice}>
+                        <NavItem className="navItem subItem">Notice Menu1</NavItem>
+                        <NavItem className="navItem subItem">Notice Menu2</NavItem>
+                        <NavItem className="navItem subItem">Notice Menu3</NavItem>
+                    </Collapse>
+
+                    <NavItem className="navItem" onClick={() => this.toggle("statistics")}><IStat/> 통계</NavItem>
+                    <Collapse isOpen={this.state.statistics}>
+                        <NavItem className="navItem subItem">Statistic Menu1</NavItem>
+                        <NavItem className="navItem subItem">Statistic Menu2</NavItem>
+                        <NavItem className="navItem subItem">Statistic Menu3</NavItem>
+                    </Collapse>
+                    <NavItem className="navItem"><ISetting/> 관리자계정</NavItem>
                 </Nav>
             </div>
         )
