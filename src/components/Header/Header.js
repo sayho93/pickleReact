7918a5e7fd2ac9a996bf371f5f78db1897606e48
sayhoChun <fishcreek@naver.com>
2017-12-22 22:05:28 +0900

    hotfix) header without bootstrap added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,79 @@
+import React from "react";
+import "./Header.css";
+import {Badge} from "reactstrap";
+
+const MenuItem = ({active, children, to}) => (
+    <div className="menu-item">
+        {children}
+    </div>
+);
+
+const LoginStatus = () => (
+    <div className="status">
+        <Badge color="danger">logout</Badge>
+    </div>
+);
+
+class Header extends React.Component{
+    componentWillMount(){
+        this.setState({
+            userId: ""
+        });
+        //adminId: cookie.load('adminId'),
+    }
+
+    onLogin(userId){
+        this.setState({
+            userId:userId
+        });
+        //cookie.save('adminId',adminId, { path: '/'});
+    }
+
+    onLogout(){
+        this.setState({
+            userId:''
+        });
+        //cookie.remove('adminId', { path: '/'});
+    }
+
+    render(){
+        return (
+            <div>
+                <div className="logo">
+                    pickleAdmin
+                    <LoginStatus/>
+                </div>
+
+                <div className="menu">
+                    <MenuItem>회원관리</MenuItem>
+                    <MenuItem>게시물관리</MenuItem>
+                    <MenuItem>알림관리</MenuItem>
+                    <MenuItem>통계</MenuItem>
+                    <MenuItem>관리자계정</MenuItem>
+                </div>
+            </div>
+        );
+    }
+}
+// const Header = () => {
+//     return (
+//         <div>
+//             <div className="logo">
+//                 pickleAdmin
+//                 <LoginStatus/>
+//             </div>
+//
+//             <div className="menu">
+//                 <MenuItem>회원관리</MenuItem>
+//                 <MenuItem>게시물관리</MenuItem>
+//                 <MenuItem>알림관리</MenuItem>
+//                 <MenuItem>통계</MenuItem>
+//                 <MenuItem>관리자계정</MenuItem>
+//             </div>
+//         </div>
+//     );
+// };
+
+export default Header;
+
+
