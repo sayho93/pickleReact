90009dba7591f0aa03d60a4fb1041bfd116ac541
sayhoChun <fishcreek@naver.com>
2017-12-28 21:35:20 +0900

    hotfix) info page input value change handled

--- src/components/userMenu/UserInfo.js
+++ src/components/userMenu/UserInfo.js
@@ -9,14 +9,12 @@ class UserInfo extends React.Component{
     constructor(props){
         super(props);
         this.state = {
-            companyInfo: {
                 name: "",
                 phone: "",
                 sidoTxt: "",
                 gunguTxt: "",
                 corporateRN: "",
                 uptDate: "",
-            }
         };
         this.fetchCompanyInfo = this.fetchCompanyInfo.bind(this);
         this.onInputChange = this.onInputChange.bind(this);
@@ -34,64 +32,50 @@ class UserInfo extends React.Component{
             return;
 
         this.setState({
-            companyInfo: {
                 name: data.name,
                 phone: data.phone,
                 sidoTxt: data.sidoTxt,
                 gunguTxt: data.gunguTxt,
                 corporateRN: data.corporateRN,
                 uptDate: data.uptDate
-            }
         });
     };
 
     onInputChange(event){
-        let companyInfo = function() {
-            let returnObj = {};
-            returnObj[this.target.name] = this.target.value;
-            return returnObj;
-        }.bind(event)();
-
-        // console.log(event.target);
-        // console.log(event.target.name);
         this.setState({
-            companyInfo
+            [event.target.name]: event.target.value
         });
-
-        // console.log(this.state);
     }
 
     render() {
-        // console.log(this.state);
-
         // if(this.state.companyInfo.name !== ""){
             return (
                 <div className="bodyArea">
                     <BreadCrumb first={"User Manage"} second={"User Info"}/>
                     <InputGroup>
                         <InputGroupAddon>이름</InputGroupAddon>
-                        <Input placeholder="company name" name="name" value={this.state.companyInfo.name} onChange={this.onInputChange}/>
+                        <Input placeholder="company name" name="name" value={this.state.name} onChange={this.onInputChange}/>
                     </InputGroup>
                     <br/>
                     <InputGroup>
                         <InputGroupAddon>전화번호</InputGroupAddon>
-                        <Input placeholder="company phone" name="phone" value={this.state.companyInfo.phone} onChange={this.onInputChange}/>
+                        <Input placeholder="company phone" name="phone" value={this.state.phone} onChange={this.onInputChange}/>
                     </InputGroup>
                     <br/>
                     <InputGroup>
                         <InputGroupAddon>주소</InputGroupAddon>
-                        <Input placeholder="company location" value={this.state.companyInfo.sidoTxt + " " +  this.state.companyInfo.gunguTxt
+                        <Input placeholder="company location" value={this.state.sidoTxt + " " +  this.state.gunguTxt
                         } readOnly/>
                     </InputGroup>
                     <br/>
                     <InputGroup>
                         <InputGroupAddon>주소</InputGroupAddon>
-                        <Input placeholder="company location" name="corporateRN" value={this.state.companyInfo.corporateRN} onChange={this.onInputChange}/>
+                        <Input placeholder="company location" name="corporateRN" value={this.state.corporateRN} onChange={this.onInputChange}/>
                     </InputGroup>
                     <br/>
                     <InputGroup>
                         <InputGroupAddon>등록일시</InputGroupAddon>
-                        <Input placeholder="company location" value={this.state.companyInfo.uptDate} readOnly/>
+                        <Input placeholder="company location" value={this.state.uptDate} readOnly/>
                     </InputGroup>
                 </div>
             )
